e719f18df951c7dde3004849e2c26f440d6c46d9
Milan Koniar <milankoniar@gmail.com>
2019-04-11 21:26:09 +0200

    created a custom pipe for formating text and one build in pipe for formatting curency

--- src/app/customers/customers.module.ts
+++ src/app/customers/customers.module.ts
@@ -2,12 +2,13 @@ import { NgModule }      from '@angular/core';
 // we dont need BrowserModule but CommonModule
 import { CommonModule } from '@angular/common';
 
+import { SharedModule } from '../shared/shared.module';
 import { CustomersComponent }  from './customers.component';
 import { CustomersListComponent } from './customers-list/customers-list.component';
 import { FilterTextboxComponent } from './customers-list/filter-textbox.component';
 
 @NgModule({
-  imports:      [ CommonModule ],
+  imports:      [ CommonModule, SharedModule ],
   declarations: [ CustomersComponent, CustomersListComponent, FilterTextboxComponent ]
   // we dont need bootstrap
   ,
