6b05cad77e2a8f89ebaf2684f14abdee5d8d6807
Milan Koniar <milankoniar@gmail.com>
2019-04-12 22:28:01 +0200

    added getCustomers and get Orders into the data service

--- src/app/core/data.service.ts
+++ src/app/core/data.service.ts
@@ -12,7 +12,29 @@ export class DataService {
 
   baseUrl: string = 'assets/';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getCustomer(id: number) : Observable<ICustomer> {
+    return this.http.get<ICustomer[]>(this.baseUrl + 'customers.json')
+      .pipe(
+        map(customers => {
+          let customer = customers.filter((cust: ICustomer) => cust.id === id);
+          return (customer && customer.length) ? customer[0] : null;
+        }),
+        catchError(this.handleError)
+      )
+  }
+
+  getOrders(id: number) : Observable<IOrder[]> {
+    return this.http.get<IOrder[]>(this.baseUrl + 'orders.json')
+      .pipe(
+        map(orders => {
+          let custOrders = orders.filter((order: IOrder) => order.customerId === id);
+          return custOrders;
+        }),
+        catchError(this.handleError)
+      );
+  }
 
   private handleError(error: any) {
     console.error('server error:', error);
